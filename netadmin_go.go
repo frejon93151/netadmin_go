d6d3d36d43bf24c23cd95934fefec39e8c19f8bd
frejon93151 <fredrik.jonsson1@halmstad.se>
2024-10-09 15:50:45 +0200

    no longer blocking netadmin_go.Models by old type Models struct

--- netadmin_go.go
+++ netadmin_go.go
@@ -6,7 +6,7 @@ package netadmin_go
 import (
 	"net/http"
 
-	Models1 "github.com/frejon93151/netadmin_go/internal/app/Models"
+	"github.com/frejon93151/netadmin_go/internal/app/Models"
 	"github.com/frejon93151/netadmin_go/internal/app/get"
 	"github.com/frejon93151/netadmin_go/internal/app/post"
 	"github.com/frejon93151/netadmin_go/internal/app/put"
@@ -26,36 +26,20 @@ var Put = putStruct{
 	Device: put.Device,
 }
 
-func NewFilterResp[T comparable]() Models1.FilterResp[T] {
-	return Models1.FilterResp[T]{}
-}
-
-type Models interface {
-	Models1.DeviceDTO
-	Models1.DeviceGetOpts
-	Models1.DevicePostOpts
-	Models1.PhysDTO
-	Models1.PhysGetOpts
+func NewFilterResp[T comparable]() Models.FilterResp[T] {
+	return Models.FilterResp[T]{}
 }
 
 type getStruct struct {
-	Devices                 func(Models1.DeviceGetOpts) (*http.Response, error)
+	Devices                 func(Models.DeviceGetOpts) (*http.Response, error)
 	DevicePhysicalInterface func(int, *int, *int) (*http.Response, error)
-	PhysicalInterfaces      func(Models1.PhysGetOpts) (*http.Response, error)
+	PhysicalInterfaces      func(Models.PhysGetOpts) (*http.Response, error)
 }
 
 type postStruct struct {
-	DeviceClone func(int, Models1.DevicePostOpts) (*http.Response, error)
+	DeviceClone func(int, Models.DevicePostOpts) (*http.Response, error)
 }
 
 type putStruct struct {
-	Device func(int, Models1.DevicePostOpts) (*http.Response, error)
-}
-
-type modelsStruct struct {
-	DeviceDTO      Models1.DeviceDTO
-	DeviceGetOpts  Models1.DeviceGetOpts
-	DevicePostOpts Models1.DevicePostOpts
-	PhysDTO        Models1.PhysDTO
-	PhysGetOpts    Models1.PhysGetOpts
+	Device func(int, Models.DevicePostOpts) (*http.Response, error)
 }
